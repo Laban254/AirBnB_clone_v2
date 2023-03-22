c4795cc469fe778a1524b3b29e36ab8f8045b615
Annette Adhiambo <annetaliya@gmail.com>
2023-03-22 09:10:37 -0700

    users

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
@@ -12,5 +13,9 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user', cascade="all, delete-orphan")
+    places = relationship(
+            'Place', backref='user',
+            cascade="all,
+            delete-orphan"
+            )
     reviews = relationship("Review", backref="user", cascade="all, delete")
