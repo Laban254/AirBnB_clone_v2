3e69eb1a776e6d3aee16a33f301448cd774f1867
laban254 <labanrotich6544@gmail.com>
2023-04-06 10:50:16 -0700

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python4
+
+"""
+Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the function do_deploy
+"""
+
+from os.path import exists
+from fabric.api import put, run, env
+
+env.hosts = ['100.25.160.128', '35.175.104.141']
+
+def do_deploy(archive_path):
+    """
+    copies archive file from local to my webservers
+    """
+
+    if not exists(archive_path):
+        return False
+    try:
+        file_name = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp/")
+
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
+
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
+
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
+            .format(file_name, file_name))
+
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
+
+        run('rm -rf /data/web_static/current')
+
+        run(('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current')
+            .format(file_name))
+
+        return True
+    except Exception:
+        return False
