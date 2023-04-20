e2bfde5005e8b50df611399c0959785f5d083ef8
laban254 <labanrotich6544@gmail.com>
2023-04-20 15:50:17 +0300

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application
+web application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns Hello HBNB!"""
+    return ("Hello HBNB!")
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Returns HBNB"""
+    return ("HBNB")
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Returns “C ” followed by the value of the text variable"""
+    return f'C {text.replace("_", " ")}'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
