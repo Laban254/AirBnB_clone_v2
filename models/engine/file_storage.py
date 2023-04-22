2885506353f44d03d5f7321034d70f3608df35a7
laban254 <labanrotich6544@gmail.com>
2023-04-22 12:26:24 -0700

    Improve engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,15 +25,11 @@ class FileStorage:
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def delete(self, obj=None):
-        """deletees an object from storage dictionary"""
+        """ deletes an object from storage dictionary """
         if (obj):
-            key = f"{obj.__class__.__name__}.{obj.id}"
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
             del FileStorage.__objects[key]
             self.save()
-
-    def close(self):
-        """Deserialize JSON file to objects"""
-        self.reload()
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -64,6 +60,10 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """calls reload method"""
+        self.reload()
